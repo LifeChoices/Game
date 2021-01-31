c7b7ea284c74d45657cb6c9d1d0c6c9cda47b0fc
James F. Thomas <james.fthomas@yahoo.com>
2021-01-30 19:17:39 -0600

    (add) App renders NavBar component for splashpage

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton
+          edge='start'
+          className={classes.menuButton}
+          color='inherit'
+          aria-label='menu'
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6' className={classes.title}>
+          News
+        </Typography>
+        <Button color='inherit'>Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
