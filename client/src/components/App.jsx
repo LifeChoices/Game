9a11f7e41c6ae5acbbd3174e5b770053d7a5d17f
James F. Thomas <james.fthomas@yahoo.com>
2021-01-30 20:28:18 -0600

    (add) App renders footer and navbar components to DOM

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <h1>We in react</h1>
+      <Footer />
     </div>
   );
 };
